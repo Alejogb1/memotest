0fb68b879ff9be0b2641c2ab606daf4362f73c99
Alejo <alejogarb25@gmail.com>
2020-10-12 11:31:37 -0300

    .

---
+++ MEMOTEST(PROPIO)/src/main.js
@@ -0,0 +1,7 @@
+let turnos = 0;
+const $tablero = document.querySelector("#tablero-juego")
+const $cuadro = $tablero.querySelectorAll(".col-juego")
+
+configurarTurno => {
+    let coloresBase = ["rojo", "amarillo", "azul", "negro", "verde"].concat(coloresBase)
+}
